310d404c17a97132f493356ff3c1e1feea653195
Flávio Oliveira <flavio_r_o@yahoo.com.br>
2021-07-07 16:10:39 -0300

    Adiciona exceptions

---
+++ ApiCatalogoJogos/Exceptions/JogoJaCadastradoException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ExemploApiCatalogoJogos.Exceptions
+{
+    public class JogoJaCadastradoException : Exception
+    {
+        public JogoJaCadastradoException() : base("Este já jogo está cadastrado")
+        { }
+    }
+}
---
+++ ApiCatalogoJogos/Exceptions/JogoNaoCadastradoException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ExemploApiCatalogoJogos.Exceptions
+{
+    public class JogoNaoCadastradoException : Exception
+    {
+        public JogoNaoCadastradoException() : base("Este jogo não está cadastrado")
+        { }
+    }
+}
