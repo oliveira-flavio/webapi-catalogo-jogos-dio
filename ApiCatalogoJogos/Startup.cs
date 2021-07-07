58ae8bb1e94dfe0edb2c736eaeca150925df4448
Flávio Oliveira <flavio_r_o@yahoo.com.br>
2021-07-07 16:12:24 -0300

    Adiciona Configure Services Add Scoped

--- ApiCatalogoJogos/Startup.cs
+++ ApiCatalogoJogos/Startup.cs
@@ -1,3 +1,6 @@
+using ApiCatalogoJogos.Services;
+using ExemploApiCatalogoJogos.Repositories;
+using ExemploApiCatalogoJogos.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +29,8 @@ namespace ApiCatalogoJogos
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IJogoService, JogoService>();
+            services.AddScoped<IJogoRepository, JogoRepository>();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
