5b623a82389a9f5bc8626f0f5192e10a2c0e1c5d
Flávio Oliveira <51276570+oliveira-flavio@users.noreply.github.com>
2022-02-01 21:14:37 -0300

    Update JogoJaCadastradoException.cs

--- ApiCatalogoJogos/Exceptions/JogoJaCadastradoException.cs
+++ ApiCatalogoJogos/Exceptions/JogoJaCadastradoException.cs
@@ -4,7 +4,7 @@ namespace ExemploApiCatalogoJogos.Exceptions
 {
     public class JogoJaCadastradoException : Exception
     {
-        public JogoJaCadastradoException() : base("Este já jogo está cadastrado")
+        public JogoJaCadastradoException() : base("Este jogo já está cadastrado")
         { }
     }
 }
